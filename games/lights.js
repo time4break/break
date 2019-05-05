fe6a5446f6848ae86657fbcff8887d869e9c09c8
time4break <49811510+time4break@users.noreply.github.com>
2019-05-05 23:48:45 +0200

    Add files via upload

--- games/lights.js
+++ games/lights.js
@@ -1,10 +1,11 @@
 export function init(){
-  var round, cell_width,grid, moves;
+  var round, cell_width,grid, moves, dgrid;
   var deg = 0;
   var resetRound = document.createElement("button"); resetRound.innerHTML = "clear round";
   var resetGame = document.createElement("button"); resetGame.innerHTML = "reset game";
   document.body.appendChild(resetRound);
   document.body.appendChild(resetGame);
+  dgrid = [];//for animations
   round = 1;
   
   function setRound(){
@@ -12,6 +13,7 @@ export function init(){
     grid = [];
     for(var i=0; i<round*round; i++){
       grid.push(0);
+      dgrid.push(0);
     }
     localStorage.setItem("lights_grid",grid.join(""));
   }
@@ -27,17 +29,24 @@ export function init(){
       ctx.moveTo(0,i*cell_width);
       ctx.lineTo(canvas.width,i*cell_width);
     }
-    ctx.stroke();
-    for(var i=0; i<round*round; i++){
-      if(grid[i] == 1){
-        var x = i%round * cell_width;
-        var y = Math.floor(i/round) * cell_width;
-        ctx.beginPath();
-        ctx.moveTo(x,y);
-        ctx.rect(x,y,cell_width,cell_width);
-        var hue = (Math.floor(i*360/(round*round)) + deg)%360;
-        ctx.fillStyle = "hsl("+hue+",50%,50%)";
-        ctx.fill();
+    ctx.stroke();  
+    var i=dgrid.length;
+    while(i--){
+      var o = dgrid[i];
+      var w = cell_width*o;
+      var of = cell_width - w;
+      var x = i%round * cell_width + of/2;
+      var y = Math.floor(i/round) * cell_width + of/2;
+      ctx.beginPath();
+      ctx.moveTo(x,y);
+      ctx.rect(x,y,w,w);
+      var hue = (Math.floor(i*360/(round*round)) + deg)%360;
+      ctx.fillStyle = "hsl("+hue+",50%,50%)";
+      ctx.fill();
+      
+      if(Math.abs(grid[i]-dgrid[i]) >= 0.01){
+        //dgrid.splice(c,1);
+        dgrid[i]+=0.03*Math.sign(grid[i]-dgrid[i]);
       }
     }
   }
@@ -49,15 +58,19 @@ export function init(){
     grid[c] = 1 - grid[c];
     if(c%round != 0){
       grid[c-1] = 1 - grid[c-1];
+      //dgrid.push([c-1,1 - grid[c-1]]);
     }
     if((c+1)%round != 0){
-      grid[c+1] = 1 - grid[c+1]
+      grid[c+1] = 1 - grid[c+1];
+      //dgrid.push([c+1,1 - grid[c+1]]);
     }
     if((c-round) >= 0){
-      grid[c-round] = 1 - grid[c-round]
+      grid[c-round] = 1 - grid[c-round];
+     // dgrid.push([c-round,1 - grid[c-round]]);
     }
     if((c + round) < round*round){
       grid[c+round] = 1 - grid[c+round];
+     // dgrid.push([c+round,1 - grid[c+round]]);
     }
     if(grid.reduce((a,b)=>{return a+b}) == grid.length){
       setTimeout(function(){
